2e25325a7733468dcffb0de49e5d64ebfb98bde7
Isabella Ramirez <irborrero@gmail.com>
2020-04-10 13:27:44 -0400

    connecting to server on heroku

--- src/app/services/QuestionServiceClient.ts
+++ src/app/services/QuestionServiceClient.ts
@@ -3,6 +3,6 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuestionServiceClient {
   findQuestionsForQuiz = (qid) =>
-    fetch(`http://localhost:3000/api/quizzes/${qid}/questions`)
+    fetch(`https://nodes-server-hw9.herokuapp.com/api/quizzes/${qid}/questions`)
       .then(response => response.json())
 }
